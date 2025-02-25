4a07b2cf3e343d64e85da06d7f5af95b6eb4c3ce
Amir Raza <mdamir.raza016@gmail.com>
2025-02-26 01:28:12 +0530

    Some changes

--- script.js
+++ script.js
@@ -15,21 +15,41 @@
 // console.log(document.body.innerHTML);
 // console.log(document.body.innerText);
 // console.log(document.body.innerHTML = '<h1>Hello, Amir Raza</h1>');
+let Username = '';
 const body = document.querySelector('body');
-console.log(body);
 body.style.backgroundImage = 'url("https://picsum.photos/1200/720")';
 body.style.backgroundSize = 'cover';
 body.style.backgroundRepeat = 'no-repeat';
+body.onload = function(){
+    Username = prompt('Please enter your name.');
+    body.innerHTML = `<h1>Hey, ${Username}</h1><button id="btn">Refresh !</button>`;
+    const h1 = document.querySelector('h1');
+    h1.style.display = 'flex';
+    h1.style.justifyContent = 'center';
+    h1.style.alignItems = 'center';
+    h1.style.marginTop = '200px';
+    h1.style.color = 'rgb(226, 226, 226)';
+    h1.style.fontSize = '50px';
+    h1.style.textAlign = 'center'; 
+    h1.style.fontFamily = 'Arial';
+    h1.style.fontWeight = 'bold';
+    h1.style.textShadow = '10px 10px 10px black';
+    h1.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
 
-const h1 = document.querySelector('h1');
-h1.style.display = 'flex';
-h1.style.justifyContent = 'center';
-h1.style.alignItems = 'center';
-h1.style.marginTop = '200px';
-h1.style.color = 'white';
-h1.style.fontSize = '50px';
-h1.style.textAlign = 'center'; 
-h1.style.fontFamily = 'Arial';
-h1.style.fontWeight = 'bold';
-h1.style.textShadow = '12px 12px 12px black';
-h1.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    const btn = document.querySelector('#btn');
+    btn.style.display = 'flex';
+    btn.style.justifyContent = 'center';
+    btn.style.justifySelf = 'center';
+    btn.style.alignItems = 'center';
+    btn.style.marginTop = '20px';
+    btn.style.backgroundColor = 'black';
+    btn.style.color = 'white';
+    btn.style.padding = '10px 32px';
+    btn.style.border = 'none';
+    btn.style.borderRadius = '5px';
+    btn.style.cursor = 'pointer';
+
+    btn.addEventListener('click', function(){
+        window.location.reload();
+    });
+    };
